5254a682d8035a1cdf750f513ba9790cbf265b17
Ildar Mulyukov <ildar@altlinux.ru>
2021-08-12 15:02:39 +0600

    remove blinky demo code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 #![no_main]
 #![no_std]
 
-use embedded_hal::digital::v2::OutputPin;
-use nrf52832_hal as hal;
-use nrf52832_hal::gpio::Level;
+use nrf52832_hal as _;
 use rtt_target::{rprint, rprintln, rtt_init, set_print_channel};
 
 #[panic_handler] // panicking behavior
@@ -31,22 +29,12 @@ fn main() -> ! {
             }
         };
     set_print_channel(rtt_channels.up.0);
-    let p = hal::pac::Peripherals::take().unwrap();
-    let port0 = hal::gpio::p0::Parts::new(p.P0);
-    let mut led = port0.p0_24.into_push_pull_output(Level::Low);
-    let mut on = true;
     let mut buf = [0u8; 255];
-    let mut cmd = rtt_channels.down.0;
+    let mut cmd_channel = rtt_channels.down.0;
 
     initial_diags();
     loop {
-        if on {
-            led.set_high().unwrap();
-        } else {
-            led.set_low().unwrap();
-        }
-        on = ! on;
-        let bufn = cmd.read(&mut buf);
+        let bufn = cmd_channel.read(&mut buf);
         if bufn > 0 {
             rprint!("{}", buf[0] as char);
         } else {
