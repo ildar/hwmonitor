9039128aba8f905999fe565d41ba69017a4ccfa1
Ildar Mulyukov <ildar@altlinux.ru>
2021-09-02 13:35:59 +0600

    text_parser.rs: extract function parse_address()

--- src/text_parser.rs
+++ src/text_parser.rs
@@ -1,16 +1,18 @@
 use hex;
 
+pub fn parse_address(s: &[u8]) -> Result<u32,&[u8]> {
+    let mut start = [0u8;4];
+    if hex::decode_to_slice(s, &mut start).is_err() {
+        return Err(b"can't decode addr");
+    };
+    Ok(u32::from_be_bytes(start))
+}
+
 // generally, command parsed: "px {} @{}"
 pub fn parse_px(s: &[u8], defaults: (u32,u32)) -> Result<(u32,u32),&[u8]>
     {
     let addr = s.split(|c| *c==b'@').last();
-    let start = if addr == None { defaults.1 } else {
-        let mut start = [0u8;4];
-        if hex::decode_to_slice(addr.unwrap(), &mut start) != Ok(()) {
-            return Err(b"can't decode addr");
-        };
-        u32::from_be_bytes(start)
-    };
+    let start = if addr == None { defaults.1 } else { parse_address(addr.unwrap())? };
     //FIXME: process 1st value
     Ok((defaults.0,start))
 }
--- testbed/src/main.rs
+++ testbed/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("run `cargo test` instead");
+    println!("run `cargo test -p testbed --target x86_64-unknown-linux-gnu` instead");
 }
 
 use kukumba::kukumba;
@@ -8,12 +8,21 @@ use text_parser::*;
 
 kukumba! (
     #[scenario_01]
-
     for_the "`text_parser` module"
-    having "example inputs" {
-        let goodcmd:[u8;15] = *b"px 20 @00000042";
+    having "example parse_address() inputs" {
+        let good1:[u8;2] = *b"42";
+    }
+    testing "parse_address() function" {
+        let res = parse_address(&good1);
+        assert_eq!(res.is_ok(), true);
+        let res = res.unwrap();
+        assert_eq!(res, 42);
     }
 
+    #[scenario_02]
+    having "example parse_px() inputs" {
+        let goodcmd:[u8;15] = *b"px 20 @00000042";
+    }
     testing "parse_px() function" {
         let res = parse_px(&goodcmd, (55,66));
         assert_eq!(res.is_ok(), true);
